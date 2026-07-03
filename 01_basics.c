054a69f80833f4a0080446ca16264c7c7a6b9861
Ayush0098 <ayushraj31072005@gmail.com>
2026-07-03 23:45:17 +0530

    Day 1

--- 01_basics.c
+++ 01_basics.c
@@ -2,3 +2,13 @@
 Day 1 of learning C
 Key Objective: Just get things installed, and get some basics of syntax 
 */
+#include <stdio.h> /*Why do we even include? and what's exactly is stdio.h?*/
+#define PI 3.14159 /*We use only 6 significant digits in a double*/
+int main() {
+    double area = 0.0, radius = 0.0;
+    printf("Enter Radius: ");
+    scanf("%lf", &radius);
+    area = PI * radius * radius;
+    printf("Radius of %lf metres, the area is %lf sq. metres", radius, area);
+    return 0;
+}
